46e69a2e7b9f446fb2bd2823127feebc9249400f
Dao Hoang Son <daohoangson@gmail.com>
2011-11-16 08:05:41 +0700

    Initial Commit

---
+++ FRD_iOS_CoreData/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FRD_iOS_CoreData
+//
+//  Created by Dao Hoang Son on 11/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FRD_iOS_CoreData/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FRD_iOS_CoreData
+//
+//  Created by Dao Hoang Son on 11/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
